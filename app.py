1cf0118c1c36d95761e2327c3f8db7db11e24e4d
CodeAx1 <122358662+CodeAx1avek@users.noreply.github.com>
2025-01-02 12:42:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def read_csv(file_path):
 @app.route('/')
 def home():
     csv_data = read_csv('social_media_data.csv')
-    return render_template('index.html', 
+    return render_template('templates/index.html', 
                        post_types=csv_data['labels'], 
                        likes_data=csv_data['likes'], 
                        shares_data=csv_data['shares'], 
